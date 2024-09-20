f972c3ab4caa8d1f913eac3919686b1ad65f87ce
Dissu <aryansi1126W@gmail.com>
2024-09-21 00:01:56 +0530

    Login FE done

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./signup.css";
+import { handleError, handleSuccess } from "../../utils";
+
+const Login = () => {
+
+  const navigate = useNavigate();
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const copyLoginInfo = { ...loginInfo};
+    copyLoginInfo[name] = value;
+    setLoginInfo(copyLoginInfo);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const { email, password } = loginInfo;
+    if(!email || !password) {
+      return handleError("Please fill all the fields");
+    }
+    try {
+      const url = "http://localhost:8080/auth/login";
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+      const result = await res.json();
+      const { success, message, jwtToken, name, error } = result;
+      if(success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000);
+      }else if(error){
+        const details = error?.details[0]?.message;
+        handleError(details);
+      }else if(!success) {
+        handleError(message);
+      }
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email..."
+            onChange={handleChange}
+            value={loginInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password..."
+            onChange={handleChange}
+            value={loginInfo.password}
+          />
+        </div>
+        <button type="submit">Login</button>
+        <span>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
